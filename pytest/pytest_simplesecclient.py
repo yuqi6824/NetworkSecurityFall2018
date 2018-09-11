77f151a49f00015324d105cbb88571405dcc0f47
yuqi6824 <yuqi6824@colorado.edu>
2018-09-11 04:14:54 -0400

    add secure_pytest

---
+++ pytest/pytest_simplesecclient.py
@@ -0,0 +1,55 @@
+#！/usr/bin/env python
+#coding:utf-8
+
+import asyncio
+import time
+import datetime
+import sys
+
+base = 'security_response_'
+counter = 0
+
+def increment():
+  global counter
+  counter = counter + 1 
+  return counter
+
+async def secure_client(message, loop, port):
+    reader, writer = await asyncio.open_connection('127.0.0.1', port,
+                                                   loop=loop)
+
+    print('Send: %r' % message)
+    writer.write(message.encode())
+    data = await reader.read(100)
+    print('Received: %r' % data.decode())
+    print('response_message has been saved')
+
+    # Create files
+    massage = data.decode()
+    i = increment()
+    file_name = base + str(i)
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    response = data.decode()
+    file = open(file_name, 'w')
+    file.write(st + '\n')
+    file.write(message + '\n')
+    file.write(response)
+    file.close()
+
+    if message == '_EXIT_':
+    	print('Close the socket')
+    	writer.close()
+
+def main(port):
+    print('Please enter this operations'+'\n'+'encrypt,P'+'\n'+'decrypt,C')
+    while(True):
+        message = input('Please enter your massage:')
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(secure_client(message, loop, port))
+        if message == '_EXIT_':
+            break
+    loop.close()
+
+if __name__ == '__main__':
+    main(*sys.argv[1:2])
---
+++ pytest/pytest_simplesecserver.py
@@ -0,0 +1,91 @@
+#！/usr/bin/env python
+#coding:utf-8
+
+import asyncio
+import sys
+import datetime
+import time
+
+base = 'security_message_'
+counter = 0
+
+def increment():
+    global counter
+    counter = counter + 1 
+    return counter
+
+def encrypt(message):
+    plaintext = message
+    cipher = ''
+    for each in plaintext:
+        c = (ord(each)+3) % 126
+        if c < 32: 
+            c+=31
+        cipher += chr(c)
+    return cipher
+
+def decrypt(message):
+    cipher = message
+    plaintext = ''
+    for each in cipher:
+        p = (ord(each)-3) % 126
+        if p < 32:
+            p+=95
+        plaintext += chr(p)
+    return plaintext
+
+async def secure_server(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+    print("Received %r from %r" % (message, addr))
+
+    #Judge encry or decry
+    if 'encrypt' in message:
+        response = 'cipher,'+encrypt(message[8:])
+        print("Send: %r" %response)
+        writer.write(response.encode())
+    elif 'decrypt' in message:
+        response = 'plaintext,'+decrypt(message[8:])
+        print("Send: %r" %response)
+        writer.write(response.encode())
+    else:
+        response = 'error input'
+        print("Send: error input")
+        writer.write(response.encode())
+    
+    #Create file
+    i = increment()
+    file_name = base + str(i)
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    file = open(file_name, 'w')
+    file.write(st + '\n')
+    file.write(message + '\n')
+    file.write(response)
+    file.close()
+    
+    print("Send: %r" % message)
+    print("Secure_message has been saved")
+    writer.write(data)
+    await writer.drain()
+    if message == '_EXIT_':
+        print("Close the socket")
+        writer.close()
+
+def main(port):
+    loop = asyncio.get_event_loop()
+    coro = asyncio.start_server(secure_server, '127.0.0.1', port, loop=loop)
+    server = loop.run_until_complete(coro)
+    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    # Close the server
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
